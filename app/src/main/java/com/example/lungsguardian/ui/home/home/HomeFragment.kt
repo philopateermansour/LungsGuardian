ca6994ec45d8c8e88c5dd4f04fc77c009654a4af
philopateermansour <philopater.mansour@gmail.com>
2024-02-21 22:52:38 +0200

    update

--- app/src/main/java/com/example/lungsguardian/ui/home/home/HomeFragment.kt
+++ app/src/main/java/com/example/lungsguardian/ui/home/home/HomeFragment.kt
@@ -1,9 +1,14 @@
 package com.example.lungsguardian.ui.home.home
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.lungsguardian.R
 import com.example.lungsguardian.databinding.FragmentHomeBinding
@@ -12,6 +17,7 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private var fileImageUser: Uri? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,9 +29,34 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        onClicks()
     }
 
+    private fun onClicks() {
+        binding.cardGallery.setOnClickListener {
+            openGallery()
+        }
+    }
+
+
+    private var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = it.data
+                if (data != null){
+                    fileImageUser = data.data
+
+                }else{
+                    Toast.makeText(activity, "Something went wrong try again", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    private fun openGallery() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        resultLauncher.launch(intent)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
