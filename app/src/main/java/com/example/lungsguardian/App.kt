caacc6dba37e2aac1cec58f5edbf2ab5cd272d39
philopateermansour <philopater.mansour@gmail.com>
2024-05-16 23:23:55 +0300

    update

--- app/src/main/java/com/example/lungsguardian/App.kt
+++ app/src/main/java/com/example/lungsguardian/App.kt
@@ -2,6 +2,8 @@ package com.example.lungsguardian
 
 import android.app.Application
 import android.content.Context
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.lungsguardian.utils.MySharedPreferences
 import dagger.hilt.android.HiltAndroidApp
 
@@ -11,5 +13,10 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         MySharedPreferences.init(this)
+        val isDarkTheme = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+
+        if (isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
